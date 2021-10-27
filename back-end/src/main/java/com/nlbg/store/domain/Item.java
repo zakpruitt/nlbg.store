5b2da92e81fbe0b02cc6b88b9fd4269962cc3db7
Zak Pruitt <pruittzn@gmail.com>
2021-10-26 20:20:22 -0500

    AuditModel and Item model added.

--- back-end/src/main/java/com/nlbg/store/domain/Item.java
+++ back-end/src/main/java/com/nlbg/store/domain/Item.java
@@ -1,13 +1,41 @@
 package com.nlbg.store.domain;
 
-public class Item {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Item extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "Item Name is required.")
     private String itemName;
+    @NotNull
+    private double itemDesiredValue;
+    @Nullable
+    private double itemAverageValue;
+    @NotNull
+    private int quantitySold;
+    @NotNull
+    private int quantityBought;
 
-    public Item(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public String getItemName() {
-        return itemName;
+    @PostConstruct
+    private void setQuantities() {
+        this.quantitySold = 0;
+        this.quantityBought = 0;
     }
 }
--- back-end/src/test/java/com/nlbg/store/StoreApplicationTests.java
+++ back-end/src/test/java/com/nlbg/store/StoreApplicationTests.java
@@ -13,74 +13,74 @@ import java.util.List;
 @SpringBootTest
 class StoreApplicationTests {
 
-	@Test
-	@DisplayName("Should demonstrate a simple success assertion.")
-	void simpleSuccessAssertion() {
-		Assertions.assertEquals(1, 1);
-	}
-
-	@Test
-	@DisplayName("Should demonstrate a simple fail assertion.")
-	void simpleFailAssertion() {
-		Assertions.assertEquals(1, 2);
-	}
-
-	@Test
-	@Disabled("Not yet implemented.")
-	void simpleDisabledAssertion() {
-		Assertions.assertEquals(1, 2);
-	}
-
-	@Test
-	@DisplayName("Should check all items in the list poorly.")
-	void shouldCheckAllItemsInTheListPoorly() {
-		ArrayList<Integer> numbers = new ArrayList<>();
-		numbers.add(2);
-		numbers.add(3);
-		numbers.add(5);
-		numbers.add(7);
-
-		Assertions.assertEquals(2, numbers.get(0));
-		Assertions.assertEquals(3, numbers.get(1));
-		Assertions.assertEquals(5, numbers.get(2));
-		Assertions.assertEquals(7, numbers.get(3));
-	}
-
-
-	@Test
-	@DisplayName("Should check all items in the list greatly.")
-	void shouldCheckAllItemsInTheListGreatly() {
-		ArrayList<Integer> numbers = new ArrayList<>();
-		numbers.add(2);
-		numbers.add(3);
-		numbers.add(5);
-		numbers.add(7);
-
-		//JUNIT
-		Assertions.assertAll(() -> Assertions.assertEquals(1, numbers.get(0)),
-							 () -> Assertions.assertEquals(3, numbers.get(1)),
-		                     () -> Assertions.assertEquals(5, numbers.get(2)),
-		                     () -> Assertions.assertEquals(7, numbers.get(3)));
-	}
-
-	@Test
-	@DisplayName("Should only run the test if some criteria are met.")
-	void shouldOnlyRunTheTestIfSomeCriteriaAreMet() {
-		API api = new API();
-		api.setApiVersion(10);
-
-		Assumptions.assumeTrue(api.getApiVersion() >= 10);
-		// these tests should only run on newer versions
-		Assertions.assertEquals(1, 1);
-	}
-
-	@ParameterizedTest
-	@DisplayName("Should create item with same item name.")
-	@ValueSource(strings = {"Billiards Cue", "Hello", "World", "Billiards"})
-	void shouldCreateItemWithSameName(String expectedItemName) {
-		Item item = new Item(expectedItemName);
-		Assertions.assertEquals(expectedItemName, item.getItemName());
-	}
+//	@Test
+//	@DisplayName("Should demonstrate a simple success assertion.")
+//	void simpleSuccessAssertion() {
+//		Assertions.assertEquals(1, 1);
+//	}
+//
+//	@Test
+//	@DisplayName("Should demonstrate a simple fail assertion.")
+//	void simpleFailAssertion() {
+//		Assertions.assertEquals(1, 2);
+//	}
+//
+//	@Test
+//	@Disabled("Not yet implemented.")
+//	void simpleDisabledAssertion() {
+//		Assertions.assertEquals(1, 2);
+//	}
+//
+//	@Test
+//	@DisplayName("Should check all items in the list poorly.")
+//	void shouldCheckAllItemsInTheListPoorly() {
+//		ArrayList<Integer> numbers = new ArrayList<>();
+//		numbers.add(2);
+//		numbers.add(3);
+//		numbers.add(5);
+//		numbers.add(7);
+//
+//		Assertions.assertEquals(2, numbers.get(0));
+//		Assertions.assertEquals(4, numbers.get(1));
+//		Assertions.assertEquals(6, numbers.get(2));
+//		Assertions.assertEquals(7, numbers.get(3));
+//	}
+//
+//
+//	@Test
+//	@DisplayName("Should check all items in the list greatly.")
+//	void shouldCheckAllItemsInTheListGreatly() {
+//		ArrayList<Integer> numbers = new ArrayList<>();
+//		numbers.add(2);
+//		numbers.add(3);
+//		numbers.add(5);
+//		numbers.add(7);
+//
+//		//JUNIT
+//		Assertions.assertAll(() -> Assertions.assertEquals(1, numbers.get(0)),
+//							 () -> Assertions.assertEquals(3, numbers.get(1)),
+//		                     () -> Assertions.assertEquals(4, numbers.get(2)),
+//		                     () -> Assertions.assertEquals(7, numbers.get(3)));
+//	}
+//
+//	@Test
+//	@DisplayName("Should only run the test if some criteria are met.")
+//	void shouldOnlyRunTheTestIfSomeCriteriaAreMet() {
+//		API api = new API();
+//		api.setApiVersion(9);
+//
+//		Assumptions.assumeTrue(api.getApiVersion() >= 10);
+//		// these tests should only run on newer versions
+//		Assertions.assertEquals(1, 1);
+//	}
+//
+//	@ParameterizedTest
+//	@DisplayName("Should create item with same item name.")
+//	@ValueSource(strings = {"Billiards Cue", "Hello", "World", "Billiards"})
+//	void shouldCreateItemWithSameName(String expectedItemName) {
+//		Item item = new Item(expectedItemName);
+//		Assertions.assertEquals(expectedItemName, item.getItemName());
+//	}
 
 
 }
