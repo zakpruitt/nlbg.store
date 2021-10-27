b37c28314ce4b96bdfe5e4431c0f42ef8e81e6b4
Zak Pruitt <pruittzn@gmail.com>
2021-10-26 20:52:47 -0500

    Item JPA Repository and MySQL remote configuration

---
+++ back-end/src/main/java/com/nlbg/store/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.nlbg.store.repository;
+
+import com.nlbg.store.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findByItemName(String itemName);
+}
