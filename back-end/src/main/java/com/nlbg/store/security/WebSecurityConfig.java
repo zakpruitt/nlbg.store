b33e8a42c34858c3e1ddaca24006b12e91bf222c
Zak Pruitt <pruittzn@gmail.com>
2021-11-14 17:46:28 -0600

    web security added with password encryption. endpoints are secured

---
+++ back-end/src/main/java/com/nlbg/store/security/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.nlbg.store.security;
+
+import com.nlbg.store.service.AdminService;
+import com.nlbg.store.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomerService customerService;
+    @Autowired
+    private AdminService adminService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/api/v*/registration/**")
+                .permitAll()
+            .anyRequest()
+        .authenticated().and()
+        .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // need to change based on user
+        auth.authenticationProvider(daoAuthenticationProviderCustomer());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProviderCustomer() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(customerService);
+        return provider;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProviderAdmin() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(adminService);
+        return provider;
+    }
+}
