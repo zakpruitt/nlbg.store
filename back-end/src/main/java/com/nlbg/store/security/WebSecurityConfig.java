427608e1eefe031ab3c5f1ff505e25d026130db2
Zak Pruitt <pruittzn@gmail.com>
2021-11-22 02:30:12 -0600

    permitted home page for unsessioned users

--- back-end/src/main/java/com/nlbg/store/security/WebSecurityConfig.java
+++ back-end/src/main/java/com/nlbg/store/security/WebSecurityConfig.java
@@ -27,12 +27,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
             .authorizeRequests()
-                .antMatchers("/api/v*/registration/**")
-                .permitAll()
-            .anyRequest()
-        .authenticated().and()
+                .antMatchers("/api/v*/registration/**").permitAll()
+                .antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+            .and()
         .formLogin();
     }
 
