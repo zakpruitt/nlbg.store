67ad3b4b304a810411283d9cded6e74aeaa69e14
Zak <pruittzn@gmail.com>
2021-11-27 03:53:11 -0600

    cloudinary wrapped into a service class

--- back-end/src/main/java/com/nlbg/store/service/PhotoService.java
+++ back-end/src/main/java/com/nlbg/store/service/PhotoService.java
@@ -7,22 +7,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @Service
 public class PhotoService {
 
+    //TODO: generate new api key and secret.
     private final Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
             "cloud_name", "dxoa7bbix",
             "api_key", "161649288458746",
             "api_secret", "eujBGLpb3B30KuJDbUcVOWAp3oA"));
 
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return args -> {
-            System.out.println("here");
-//            cloudinary.uploader().upload(new File("C:/Program Files (x86)/World of Warcraft/_classic_era_/Screenshots/awesome.jpg"),
-//                    ObjectUtils.asMap("public_id", "olympic_flag"));
-        };
+    public void uploadImage(String imageURL, String publicId) throws IOException {
+        cloudinary.uploader().upload(new File(imageURL), ObjectUtils.asMap("public_id", "olympic_flag"));
+    }
+
+    public String retrieveImageURL(String publicId) throws Exception {
+        Map result = cloudinary.api().resource("olympic_flag", ObjectUtils.emptyMap());
+        return result.get("secure_url").toString();
     }
 }
