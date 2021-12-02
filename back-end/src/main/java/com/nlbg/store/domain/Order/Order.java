0e2d9c133c356ab4b0d16eda6a8601f44a060ced
Zak Pruitt <pruittzn@gmail.com>
2021-12-01 20:44:15 -0600

    removed order misc columns

--- back-end/src/main/java/com/nlbg/store/domain/Order/Order.java
+++ back-end/src/main/java/com/nlbg/store/domain/Order/Order.java
@@ -63,22 +63,6 @@ public class Order extends AuditModel {
         this.orderStatus = orderStatus;
     }
 
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
     public Item getItem() {
         return item;
     }
