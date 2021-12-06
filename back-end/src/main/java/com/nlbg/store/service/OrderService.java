afb4a561130f659f528e83b1c077df602954abeb
Zak Pruitt <pruittzn@gmail.com>
2021-12-06 17:16:23 -0600

    paypal config and service added

--- back-end/src/main/java/com/nlbg/store/service/OrderService.java
+++ back-end/src/main/java/com/nlbg/store/service/OrderService.java
@@ -5,29 +5,35 @@ import com.nlbg.store.domain.Order.Order;
 import com.nlbg.store.domain.Order.SellOrderForm;
 import com.nlbg.store.domain.Order.ShippingInformation;
 import com.nlbg.store.domain.User.Customer;
-import com.nlbg.store.repository.ItemRepository;
 import com.nlbg.store.repository.OrderRepository;
 import com.nlbg.store.repository.ShippingInformationRepository;
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class OrderService {
 
     @Autowired
-    OrderRepository orderRepository;
+    private OrderRepository orderRepository;
     @Autowired
-    ShippingInformationRepository shippingInformationRepository;
-
+    private ShippingInformationRepository shippingInformationRepository;
     @Autowired
-    ItemService itemService;
+    private APIContext apiContext;
     @Autowired
-    PhotoService photoService;
+    private ItemService itemService;
+    @Autowired
+    private PhotoService photoService;
 
     public void createSellOrder(SellOrderForm sellOrderForm, Customer customer) throws IOException {
         Item item = itemService.getItemByName(sellOrderForm.getItemName());
@@ -57,4 +63,39 @@ public class OrderService {
         orderRepository.save(order);
     }
 
+    public Payment createPayment(Double total, String currency, String method, String intent, String description, String cancelUrl, String successUrl) throws PayPalRESTException {
+        Amount amount = new Amount();
+        amount.setCurrency(currency);
+        total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
+        amount.setTotal(String.format("%.2f", total));
+
+        Transaction transaction = new Transaction();
+        transaction.setDescription(description);
+        transaction.setAmount(amount);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod(method);
+
+        Payment payment = new Payment();
+        payment.setIntent(intent);
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl(cancelUrl);
+        redirectUrls.setReturnUrl(successUrl);
+        payment.setRedirectUrls(redirectUrls);
+
+        return payment.create(apiContext);
+    }
+
+    public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException{
+        Payment payment = new Payment();
+        payment.setId(paymentId);
+        PaymentExecution paymentExecute = new PaymentExecution();
+        paymentExecute.setPayerId(payerId);
+        return payment.execute(apiContext, paymentExecute);
+    }
 }
